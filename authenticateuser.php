1bbfbd2f0a195ffc8d3f1bfe288255a8fc1f6ba4
BreethiSundar <66816394+BreethiSundar@users.noreply.github.com>
2020-06-13 20:26:38 +0530

    Update authenticateuser.php

--- authenticateuser.php
+++ authenticateuser.php
@@ -3,6 +3,7 @@
   include 'utility.php';
   getData($username);
   getData($password);
+  $sql="select * from 
   if(($username=='username') && ($password=='password'))
   {
     header("Location: index.php");
