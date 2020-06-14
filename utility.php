cc99de6035db56661712868a2bd50d56e701ae5a
demontricer <38652605+demontricer@users.noreply.github.com>
2020-06-14 06:40:41 +0530

    Update utility.php

--- utility.php
+++ utility.php
@@ -1,18 +1,19 @@
 <?php
-  function getData($str)
+  function getData($connect,$str)
   {
     if(isset($_POST[$str]))
-       {
-          $str1=$str;   
-       }
+     {
+        $str1=$str;   
+     }
     else if(isset($_GET[$str]))
-       {
-          $str1=$str;   
-       }
+     {
+        $str1=$str;   
+     }
     else
-       {
-          $str1='null';
-          
-       }
+     {
+        $str1=NULL;
+     }
+    $str1=mysqli_real_escape_string($connect,$str1);
+    return($str1);
   }
 ?>
